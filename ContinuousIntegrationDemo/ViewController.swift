d2b100814c5263b12bc9d3c0ca227862c95b73b6
Dhara Patel <dhara.patel@letsnurture.com>
2018-05-25 11:00:46 +0530

    Initial Commit

---
+++ ContinuousIntegrationDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ContinuousIntegrationDemo
+//
+//  Created by LN-MCMI-006 on 25/05/18.
+//  Copyright © 2018 LN-MCMI-006. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
